6457b88e88b8c52284ef7f352cd8a1932f4767da
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2022-05-01 10:04:09 +0800

    add

--- src/components/Col.js
+++ src/components/Col.js
@@ -1,14 +1,18 @@
 import styled from "styled-components/macro";
 import { getColWidth } from "../utils";
 
-export const Col = styled.div`
-  position: relative;
-  min-height: 1px;
-  padding-left: 15px;
-  padding-right: 15px;
-  height: inherit;
-  float: left;
-  ${(props) => {
-    return getColWidth(props);
-  }}
-`;
+// export const Col = styled.div`
+//   position: relative;
+//   min-height: 1px;
+//   padding-left: 15px;
+//   padding-right: 15px;
+//   height: inherit;
+//   float: left;
+//   ${(props) => {
+//     return getColWidth(props);
+//   }}
+// `;
+
+export const Col = ({ children, xs, md }) => {
+  return <div>{children}</div>;
+};
